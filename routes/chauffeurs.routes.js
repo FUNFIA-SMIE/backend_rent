b437efc64adc4226e3f54779b780bf96d1acb946
FUNFIA-SMIE <informaticienfunfiasmie@gmail.com>
2026-02-02 16:29:07 +0300

    adf

--- routes/chauffeurs.routes.js
+++ routes/chauffeurs.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db'); // Import PostgreSQL config
+const db = require('../config/db'); // Import PostgreSQsL config
 
 // Create - Ajouter un chauffeur
 router.post('/', async (req, res) => {
